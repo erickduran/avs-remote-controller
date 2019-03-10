ca1e761e49e58bf053ba81a43860b66ef97eb7ea
CesarTorres2596 <37921177+CesarTorres2596@users.noreply.github.com>
2019-03-10 00:20:37 -0800

    validate_raw and validate finished

--- remote-controller/remote_controller/command_validator.py
+++ remote-controller/remote_controller/command_validator.py
@@ -6,10 +6,25 @@ class CommandValidator:
 
     def validate_raw(self, command):
         # just check if command exists in raw-commands.yml
-        pass
+        if command in self.__raw_commands['commands'].keys():
+            print(command + " is a raw_command")
+        else:
+            pass
 
     def validate(self, command, value):
         # check if command exists in commands.yml
         # check if args are required
         # check if value is an int when applies
-        pass
+
+        if command in self.__commands['commands'].keys():
+            print(command + " is a command")
+        try:
+            if(str.isdecimal(value) == False):
+                print("Given argument is not decimal")
+            if  self.__commands['commands'][command]['args']['OPTIONAL'] == False:
+                print("Command requiere argument")
+            else:
+                print("Command with optional arguments")
+
+        except:
+            print("Command without optional arguments")
