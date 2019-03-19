f4f548c6e72ce60d798823558ff6842c028ad6db
Sebastian Perez <sebastian.perez@cetys.edu.mx>
2019-03-19 00:50:27 -0700

    Commands and raw commands added

--- remote-controller/tests/ir_sender_test.py
+++ remote-controller/tests/ir_sender_test.py
@@ -23,6 +23,8 @@ class IRSenderTest(unittest.TestCase):
         self.__device = 'lg'
         self.__ir_sender = IRSender(self.__device, actions, True)
 
+    # Commands
+    # Power
     def test_send_raw_power_command(self):
         command = 'KEY_POWER'
         expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
@@ -35,106 +37,11 @@ class IRSenderTest(unittest.TestCase):
         result = self.__ir_sender.send(command, None)
         self.assertTrue(expected, result)
 
-    def test_send_raw_command(self):
-        command = 'KEY_VOLUMEDOWN'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, '')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_volumedown_command(self):
-        command = 'VOLUME_DOWN'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'VOLUME_DOWN')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_volumeup_command(self):
-        command = 'VOLUME_UP'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'VOLUME_UP')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_volumeup_command(self):
-        command = 'KEY_VOLUMEUP'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'KEY_VOLUMEUP')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_channeldown_command(self):
-        command = 'KEY_CHANNELDOWN'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_CHANNELDOWN')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_channeldown_command(self):
-        command = 'CHANNEL_DOWN'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'CHANNEL_DOWN')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_channelup_command(self):
-        command = 'KEY_CHANNELUP'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_CHANNELUP')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_channelup_command(self):
-        command = 'CHANNEL_UP'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, '')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_up_command(self):
-        command = 'KEY_UP'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_UP')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_up_command(self):
-        command = 'MOVE_UP'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_UP')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_down_command(self):
-        command = 'KEY_DOWN'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_DOWN')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_down_command(self):
-        command = 'MOVE_DOWN'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_DOWN')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_right_command(self):
-        command = 'KEY_RIGHT'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_RIGHT')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_right_command(self):
-        command = 'MOVE_RIGHT'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_RIGHT')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_raw_left_command(self):
-        command = 'RAW_LEFT'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'RAW_LEFT')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
-    def test_send_left_command(self):
-        command = 'MOVE_LEFT'
-        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_LEFT')]
-        result = self.__ir_sender.send(command, None)
-        self.assertTrue(expected, result)
-
+    # Ok
     def test_send_raw_ok_command(self):
         command = 'KEY_OK'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_OK')]
-        result = self.__ir_sender.send(command, None)
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
         self.assertTrue(expected, result)
 
     def test_send_ok_command(self):
@@ -143,21 +50,245 @@ class IRSenderTest(unittest.TestCase):
         result = self.__ir_sender.send(command, None)
         self.assertTrue(expected, result)
 
+    # List
+    def test_send_raw_list_command(self):
+        command = 'KEY_LIST'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
 
-
-    def test_send_raw_key0_command(self):
-        command = 'KEY_0'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_0')]
+    def test_send_list_command(self):
+        command = 'LIST'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'LIST')]
         result = self.__ir_sender.send(command, None)
         self.assertTrue(expected, result)
 
-    def test_send_raw_key1_command(self):
-        command = 'KEY_1'
-        expected = ['irsend SEND_ONCE{} {}'.format(self.__device, 'KEY_1')]
+    # Undo
+    def test_send_raw_undo_command(self):
+        command = 'KEY_UNDO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_undo_command(self):
+        command = 'UNDO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'UNDO')]
         result = self.__ir_sender.send(command, None)
         self.assertTrue(expected, result)
+
+    # Back
+    def test_send_raw_back_command(self):
+        command = 'KEY_BACK'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_back_command(self):
+        command = 'BACK'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'BACK')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Info
+    def test_send_raw_info_command(self):
+        command = 'KEY_INFO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_info_command(self):
+        command = 'INFO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'INFO')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Exit
+    def test_send_raw_exit_command(self):
+        command = 'KEY_EXIT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_exit_command(self):
+        command = 'EXIT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'EXIT')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Menu
+    def test_send_raw_menu_command(self):
+        command = 'KEY_MENU'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_menu_command(self):
+        command = 'MENU'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MENU')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Mute
+    def test_send_raw_mute_command(self):
+        command = 'KEY_MUTE'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_mute_command(self):
+        command = 'MUTE'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MUTE')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Config
+    def test_send_raw_config_command(self):
+        command = 'KEY_CONFIG'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_config_command(self):
+        command = 'SETTINGS'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'SETTINGS')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Video
+    def test_send_raw_video_command(self):
+        command = 'KEY_VIDEO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_config_command(self):
+        command = 'INPUT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'INPUT')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Screen
+    def test_send_raw_screen_command(self):
+        command = 'KEY_SCREEN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_screen_command(self):
+        command = 'SCREEN_RATIO'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'SCREEN_RATIO')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Volume
+    # Up
+    def test_send_raw_volumeup_command(self):
+        command = 'KEY_VOLUMEUP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_volumeup_command(self):
+        command = 'VOLUME_UP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'VOLUME_UP')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Down
+    def test_send_raw_volumedown_command(self):
+        command = 'KEY_VOLUMEDOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_volumedown_command(self):
+        command = 'VOLUME_DOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'VOLUME_DOWN')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Channel
+    # Up
+    def test_send_raw_channelup_command(self):
+        command = 'KEY_CHANNELUP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_channelup_command(self):
+        command = 'CHANNEL_UP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'CHANNEL_UP')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+    # Down
+    def test_send_raw_channeldown_command(self):
+        command = 'KEY_CHANNELDOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_channeldown_command(self):
+        command = 'CHANNEL_DOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'CHANNEL_DOWN')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Directions
+    # Up
+    def test_send_raw_keyup_command(self):
+        command = 'KEY_UP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_keyup_command(self):
+        command = 'MOVE_UP'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_UP')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Down
+    def test_send_raw_keydown_command(self):
+        command = 'KEY_DOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_keydown_command(self):
+        command = 'MOVE_DOWN'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_DOWN')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Right
+    def test_send_raw_keyright_command(self):
+        command = 'KEY_RIGHT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_keyright_command(self):
+        command = 'MOVE_RIGHT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_RIGHT')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    # Left
+    def test_send_raw_keyleft_command(self):
+        command = 'KEY_LEFT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, command)]
+        result = self.__ir_sender.send_raw(command)
+        self.assertTrue(expected, result)
+
+    def test_send_keyleft_command(self):
+        command = 'MOVE_LEFT'
+        expected = ['irsend SEND_ONCE {} {}'.format(self.__device, 'MOVE_LEFT')]
+        result = self.__ir_sender.send(command, None)
+        self.assertTrue(expected, result)
+
+    
 
     # TODO
-    # test more commands and raw commands
-    # test composite repetition commands (volume up, volume down, etc.)
+    # test more commands and raw commands                               Done
+    # test composite repetition commands (volume up, volume down, etc.) Tbd
     # test composite number commands (channel)
