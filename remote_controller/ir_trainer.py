f3ccb105ac473be2a1dc42c144c63577189546f2
CESARIUX2596 <cesaralan10@hotmail.com>
2019-03-25 15:15:23 -0700

    Creation of ir_trainer to add new devices to lircd configuration

--- remote_controller/ir_trainer.py
+++ remote_controller/ir_trainer.py
@@ -2,10 +2,15 @@ import os
 
 
 class IRTrainer:
-
-    def __init__(self, review_mode):
-        self.__review_mode = review_mode
-
-    def train(self):
-        bash_command = 'irrecord -d /dev/lirc0'
-        pass
+    @staticmethod
+    def ir_trainer():
+        bash_kill_command = 'sudo service lircd stop'
+        print('killing lircd service: ')
+        print(bash_kill_command)
+        os.system(bash_kill_command)
+        name = input('Name of the new device: (name will be converted to lowercase)')
+        name = name.lower()
+        bash_train_command = 'irrecord -d /dev/lirc0'
+        os.system(bash_train_command)
+        bash_copy_command = 'sudo cp {}.lircd.conf /etc/lirc/lircd.conf.d/{}.lircd.conf'.format(name, name)
+        os.system(bash_copy_command)
