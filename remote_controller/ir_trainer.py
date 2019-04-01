8ed93315e48b6655f6b447d3e31316e9c59c82ee
erick <erick.duran@cetys.edu.mx>
2019-04-01 01:01:49 -0700

    AVS-33: adding training mode to cli

--- remote_controller/ir_trainer.py
+++ remote_controller/ir_trainer.py
@@ -2,15 +2,18 @@ import os
 
 
 class IRTrainer:
+
     @staticmethod
-    def ir_trainer():
-        bash_kill_command = 'sudo service lircd stop'
-        print('killing lircd service: ')
-        print(bash_kill_command)
-        os.system(bash_kill_command)
-        name = input('Name of the new device: (name will be converted to lowercase)')
+    def run():
+        print('Please stop the lirc service by running:')
+        print('sudo service lircd stop')
+
+        name = input('Name of the new device: ')
         name = name.lower()
-        bash_train_command = 'irrecord -d /dev/lirc0'
-        os.system(bash_train_command)
-        bash_copy_command = 'sudo cp {}.lircd.conf /etc/lirc/lircd.conf.d/{}.lircd.conf'.format(name, name)
-        os.system(bash_copy_command)
+
+        command = 'irrecord -d /dev/lirc0'
+        os.system(command)
+
+        copy_command = 'sudo cp {}.lircd.conf /etc/lirc/lircd.conf.d/{}.lircd.conf'.format(name, name)
+        print('Please copy your lirc configuration file by running:')
+        print(copy_command)
